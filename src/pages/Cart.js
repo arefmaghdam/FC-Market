778f908d141dabce514a0553b5e6a6d0e609d9a3
arefmaghdam <arefmaghdam@gmail.com>
2023-11-14 02:56:16 +0330

    saved selected products to local storage

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,38 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Row, Col, ListGroup, Image, Button } from "react-bootstrap";
 
 const Cart = () => {
   const selectedProduct = useSelector((state) => state.selectedProduct.value);
   useEffect(() => {
-    console.log(selectedProduct);
-  }, [selectedProduct]);
-  return <div>{selectedProduct.name}</div>;
+    if(localStorage.getItem("products")) return
+    let currentCartItems = [];
+    currentCartItems = localStorage.setItem("products", JSON.stringify([]));
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(
+      "products",
+      JSON.stringify([
+        ...JSON.parse(localStorage.getItem("products")),
+        selectedProduct,
+      ])
+    );
+  }, []);
+  return (
+    <div>
+      <Row>
+        <Col md={8}>
+          <h2>سبد خرید</h2>
+          {selectedProduct === undefined ? (
+            <p>سبد خرید خالی است</p>
+          ) : (
+            <ListGroup variant="flush"></ListGroup>
+          )}
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Cart;
